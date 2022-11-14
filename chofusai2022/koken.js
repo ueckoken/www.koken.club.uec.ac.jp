37251b3711880e90a94ef1fc9704e1a9347b48f0
otariidae <otariidae@users.noreply.github.com>
2022-11-14 23:19:39 +0900

    Twitterウィジェットの高さを親要素に合わせる

--- chofusai2022/koken.js
+++ chofusai2022/koken.js
@@ -1,17 +1,4 @@
-window.addEventListener("load",twitter)
-window.addEventListener("resize",twitter)
 window.addEventListener("load",navmenu)
-
-/*Twitterタイムライン格闘 with yuiくん*/
-function twitter(){
-    if (document.body.clientWidth > 1200){
-        let h = document.getElementById("text").clientHeight;
-        document.getElementById("twittercontents").setAttribute("data-height",h+"px")
-    }
-    else {
-        document.getElementById("twittercontents").setAttribute("data-height","500px")
-    }
-}
 
 let hamburger;
 let nav;
